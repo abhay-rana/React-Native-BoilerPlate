1caa1384ee0ecdcf7ffdb7fce65e88d8f7dd7795
abhay-rana <abhayrana286@gmail.com>
2023-07-15 19:25:42 +0530

    changes to the boilerplate

--- template.config.js
+++ template.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     // Placeholder name that will be replaced in package.json, index.json, android/, ios/ for a project name.
     placeholderName: 'rn-template',
-    templateDir: './template',
+    templateDir: './boilerplate',
 };
