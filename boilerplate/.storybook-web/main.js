3127c54ee6da5210c1717bae3aaaa5f287ab9ec8
abhay-rana <abhayrana286@gmail.com>
2023-07-19 23:08:15 +0530

    storybook integrations

---
+++ boilerplate/.storybook-web/main.js
@@ -0,0 +1,12 @@
+// ./storybook-web/main.js
+module.exports = {
+    stories: ['../.storybook/stories/**/*.stories.?(ts|tsx|js|jsx)'],
+    addons: [
+        '@storybook/addon-essentials',
+        '@storybook/addon-react-native-web',
+    ],
+    core: {
+        builder: 'webpack5',
+    },
+    framework: '@storybook/react',
+};
